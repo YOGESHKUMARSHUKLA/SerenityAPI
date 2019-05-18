109920e30af4d6b6b42dae97e3a10930b9ea5715
Yogesh Shukla <Yogesh1#0>
2019-05-18 15:21:05 +0530

    test2

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -7,6 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = "src/test/resources/features"
+        features = "src/test/resources/features",
+        glue = {"starter.stepdefinitions"}
+
 )
 public class CucumberTestSuite {}
--- src/test/java/starter/StepDefinitions.java
+++ src/test/java/starter/StepDefinitions.java
@@ -3,9 +3,13 @@ package starter;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import cucumber.api.java.en.And;
+import net.thucydides.core.annotations.Steps;
+import starter.navigation.DuckDuckGoHomePage;
 
 public class StepDefinitions {
+    @Steps
+    DuckDuckGoHomePage home;
+
     @Given("^I am thirsty$")
     public void i_am_thirsty() throws Throwable {
     }
@@ -17,5 +21,23 @@ public class StepDefinitions {
     @Then("^I should not receive a latte$")
     public void i_should_not_receive_a_latte() throws Throwable {
     }
+//
+//    @Given("^Sergey is on the DuckDuckGo home page$")
+//    public void sergey_is_on_the_DuckDuckGo_home_page() {
+//        // Write code here that turns the phrase above into concrete actions
+//        home.me();
+//    }
+//
+//    @When("^he searches for \"([^\"]*)\"$")
+//    public void he_searches_for(String arg1) {
+//        // Write code here that turns the phrase above into concrete actions
+//        home.me();
+//    }
+//
+//    @Then("^all the result titles should contain the word \"([^\"]*)\"$")
+//    public void all_the_result_titles_should_contain_the_word(String arg1) {
+//        // Write code here that turns the phrase above into concrete actions
+//        home.me();
+//    }
 
 }
